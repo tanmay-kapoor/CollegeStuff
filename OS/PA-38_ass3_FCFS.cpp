ddeab2d9a9d0797e91d1bbe5484c87912d0ebccc
Tanmay Kapoor <54765593+tanmay-kapoor@users.noreply.github.com>
2020-05-24 19:14:08 +0530

    Update PA-38_ass3_FCFS.cpp

--- OS/PA-38_ass3_FCFS.cpp
+++ OS/PA-38_ass3_FCFS.cpp
@@ -1,65 +1,52 @@
 //PA 38
 //Tanmay Kapoor
 #include<stdio.h>
-int main()
-{
 
-int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0};
-int n,sum=0;
-float total_TAT=0,total_WT=0;
+int main() {
+    int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0};
+    int n,sum=0;
+    float total_TAT=0,total_WT=0;
 
-printf("Enter no. of processes\n");
-scanf("%d",&n);
+    printf("Enter no. of processes\n");
+    scanf("%d",&n);
 
-printf("Enter arrival time and burst time for each process\n");
+    printf("Enter arrival time and burst time for each process\n");
 
-for(int i=0;i<n;i++)
-{
+    for(int i=0;i<n;i++) {
+        printf("Arrival time of process[%d] ",i+1);
+        scanf("%d",&at[i]);
+        printf("Burst time of process[%d] ",i+1);
+        scanf("%d",&bt[i]);
+        printf("\n");
+    }
 
-printf("Arrival time of process[%d] ",i+1);
-scanf("%d",&at[i]);
+    //calculate completion time of processes
+    for(int j=0;j<n;j++) {
+        sum+=bt[j];
+        ct[j]+=sum;
+    }
 
-printf("Burst time of process[%d] ",i+1);
-scanf("%d",&bt[i]);
+    //calculate turnaround time and waiting times
+    for(int k=0;k<n;k++) {
+        tat[k]=ct[k]-at[k];
+        total_TAT+=tat[k];
+    }
 
-printf("\n");
-}
+    for(int k=0;k<n;k++) {
+        wt[k]=tat[k]-bt[k];
+        total_WT+=wt[k];
+    }
 
-//calculate completion time of processes
+    printf("Solution: \n\n");
+    printf("P#\t AT\t BT\t CT\t TAT\t WT\t\n\n");
 
-for(int j=0;j<n;j++)
-{
-sum+=bt[j];
-ct[j]+=sum;
-}
+    for(int i=0;i<n;i++)
+        printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
 
-//calculate turnaround time and waiting times
+    printf("\n\nAverage Turnaround Time = %f\n",totalTAT/n);
+    printf("Average WT = %f\n\n",totalWT/n);
 
-for(int k=0;k<n;k++)
-{
-tat[k]=ct[k]-at[k];
-total_TAT+=tat[k];
-}
-
-
-for(int k=0;k<n;k++)
-{
-wt[k]=tat[k]-bt[k];
-total_WT+=wt[k];
-}
-
-printf("Solution: \n\n");
-printf("P#\t AT\t BT\t CT\t TAT\t WT\t\n\n");
-
-for(int i=0;i<n;i++)
-{
-printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
-}
-
-printf("\n\nAverage Turnaround Time = %f\n",totalTAT/n);
-printf("Average WT = %f\n\n",totalWT/n);
-
-return 0;
+    return 0;
 }
 /* output
 Enter number of processes       3
